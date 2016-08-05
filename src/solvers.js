79142866d04a8460e8d7865568802006be50eafa
Brian Kilrain <bkilrain@gmail.com>
2016-08-04 22:50:34 -0700

    Fix Recursive solution that checks with binary patterns

--- src/solvers.js
+++ src/solvers.js
@@ -55,26 +55,9 @@ window.findNQueensSolution = function(n) {
   var getOptions = function(n) {
     opts = [];
     for (var i = 0; i < n; i++) {
-      opts.push(parseInt(Math.pow(10, i)));
+      opts.push(parseInt(Math.pow(10, i), 2));
     }
-
     return opts;
-
-
-    // var results = [];
-
-    // for (var i = 0; i < n; i++) {
-    //   var result = [];
-    //   for (var j = 0; j < n; j++) {
-    //     if (i === j) {
-    //       result.push(1);
-    //     } else {
-    //       result.push(0);
-    //     }
-    //   }
-    //   results.push(result);
-    // }
-    // return results;
   };
   var options = getOptions(n);
 
@@ -83,7 +66,8 @@ window.findNQueensSolution = function(n) {
     if (currentOptions.length === 0) {
       var solved = currentResults.map(function(item) {
         var row = item.toString(2).split('');
-        return new Array(n - row.length + 1).join('0').split('').concat(row);
+        var arr = new Array(n - row.length + 1).join('0').split('').concat(row);
+        return arr;
       });
       solution = solved.map(function(row) {
         return row.map(function(item) {
@@ -92,51 +76,22 @@ window.findNQueensSolution = function(n) {
       }); 
       return;
     } else {
-      var nextOption = true;
       for (var i = 0; i < currentOptions.length; i++) {
+        var nextOption = true;
         for (var j = 0; j < currentResults.length; j++) {
           var low = Math.min(currentOptions[i], currentResults[j]);
           var high = Math.max(currentOptions[i], currentResults[j]);
-          if (Math.pow(low, 2 * (currentResults.length - j)) === high) {
+          if (low * Math.pow(2, (currentResults.length - j)) === high) {
             nextOption = false;
             break;
-          } 
-        } 
-        if (nextOption) {
+          }
+        } if (nextOption) {
           var checkOption = currentOptions.splice(i, 1);
           recurse(currentOptions, currentResults.concat(checkOption));
           currentOptions.splice(i, 0, checkOption[0]);
         }
       }
     }
-
-
-
-
-
-
-
-
-
-
-    // if (currentOptions.length === 0) {
-    //   var check = new Board(currentResults);
-    //   if (!check.hasAnyQueensConflicts()) {
-    //     solution = currentResults.slice();
-    //   }
-    //   return;
-    // } else {
-    //   for (var i = 0; i < currentOptions.length; i++) {
-    //     var checkOpDec = parseInt(currentOptions[i].join(''), 2);
-    //     var lastOpDec = currentResults.length > 0 ? parseInt(currentResults[currentResults.length - 1].join(''), 2) : undefined;
-    //     if (checkOpDec / 2 === lastOpDec || checkOpDec * 2 === lastOpDec) {
-    //       continue;
-    //     }
-    //     var checkOption = currentOptions.splice(i, 1);
-    //     recurse(currentOptions, currentResults.concat(checkOption));
-    //     currentOptions.splice(i, 0, checkOption[0]);
-    //   }
-    // }
   };
 
   recurse(options, []);
@@ -152,42 +107,34 @@ window.countNQueensSolutions = function(n) {
   var solutionCount = 0; 
 
   var getOptions = function(n) {
-    var results = [];
-
+    opts = [];
     for (var i = 0; i < n; i++) {
-      var result = [];
-      for (var j = 0; j < n; j++) {
-        if (i === j) {
-          result.push(1);
-        } else {
-          result.push(0);
-        }
-      }
-      results.push(result);
+      opts.push(parseInt(Math.pow(10, i), 2));
     }
-    return results;
+    return opts;
   };
   var options = getOptions(n);
 
 
   var recurse = function(currentOptions, currentResults) {
     if (currentOptions.length === 0) {
-      var check = new Board(currentResults);
-      if (!check.hasAnyQueensConflicts()) {
-        solutionCount++;
-      }
+      solutionCount++;
       return;
     } else {
       for (var i = 0; i < currentOptions.length; i++) {
-        var checkOpDec = parseInt(currentOptions[i].join(''), 2);
-        var lastOpDec = currentResults.length > 0 ? parseInt(currentResults[currentResults.length - 1].join(''), 2) : undefined;
-        if (checkOpDec / 2 === lastOpDec || checkOpDec * 2 === lastOpDec) {
-          continue;
+        var nextOption = true;
+        for (var j = 0; j < currentResults.length; j++) {
+          var low = Math.min(currentOptions[i], currentResults[j]);
+          var high = Math.max(currentOptions[i], currentResults[j]);
+          if (low * Math.pow(2, (currentResults.length - j)) === high) {
+            nextOption = false;
+            break;
+          }
+        } if (nextOption) {
+          var checkOption = currentOptions.splice(i, 1);
+          recurse(currentOptions, currentResults.concat(checkOption));
+          currentOptions.splice(i, 0, checkOption[0]);
         }
-
-        var checkOption = currentOptions.splice(i, 1);
-        recurse(currentOptions, currentResults.concat(checkOption));
-        currentOptions.splice(i, 0, checkOption[0]);
       }
     }
   };
